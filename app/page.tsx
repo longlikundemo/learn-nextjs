506b0d0fd1ebab0d0b1c5afcc2b0eb6a1493718d
longlikundemo <longlikundemo@gmail.com>
2024-10-11 21:03:38 +0800

    finish

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,13 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
-import styles from '@/app/ui/home.module.css';
+// import styles from '@/app/ui/home.module.css';
 import Image from 'next/image';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: '控制台',
+};
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
